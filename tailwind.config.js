4de972c44e62789c145a1a238bdc20e3069935bf
Privatik3 <lol11999333@gmail.com>
2022-05-10 21:49:54 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/pages/*.html',
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '10p': '10px'
+      }
+    },
+  },
+  plugins: [],
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/pages/home.html' }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'src'),
+        port: 8080,
+        open: true,
+        hot: true
+    }
+};
