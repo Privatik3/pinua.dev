9ee621641ba0f0a8082faf44dced4be8f64e7cc4
Privatik3 <lol11999333@gmail.com>
2022-05-11 19:49:30 +0300

    Privatik3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
+const spacingGen = () => {
+  let result = {}
+  for (let i = 2; i < 400; i++) {
+    result[`${i}p`] = `${i}px`;
+  }
+
+  return result;
+};
+
 module.exports = {
   content: [
     './src/pages/*.html',
   ],
   theme: {
+    screens: {
+      'lg': {'max': '1119px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '424px'}
+    },
     extend: {
-      spacing: {
-        '10p': '10px'
+      fontSize: {
+        '18p': ['18px', {
+          lineHeight: '27px',
+        }],
       },
+      spacing: spacingGen(),
       fontFamily: {
         'Roboto': ['Roboto', 'sans-serif']
       },
