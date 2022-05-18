c8b8ba40d550e7af0e3fd22d31dd80bfb3505d7b
Администратор <alexanderpavlovfreelance@gmail.com>
2022-05-18 21:25:09 +0300

    XIIIAngel

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/pages/news.html' }),
+        new HtmlWebpackPlugin({ template: './src/pages/home.html' }),
     ],
     devServer: {
         static: path.resolve(__dirname, 'src'),
